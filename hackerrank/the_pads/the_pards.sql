6d814faabe75fc2b51b6fc561c1037e8f628f407
Omer Farooq Ahmed <omerfarooqali@gmail.com>
2023-02-02 13:23:46 +0000

    the pads sql query added

---
+++ hackerrank/the_pads/the_pards.sql
@@ -0,0 +1,7 @@
+SELECT CONCAT(Name,"(",LEFT(Occupation,1),")") 
+FROM OCCUPATIONS 
+ORDER BY Name;
+
+SELECT CONCAT("There are a total of ", COUNT(Occupation), " ", LOWER(Occupation), "s.") FROM OCCUPATIONS 
+GROUP BY Occupation 
+ORDER BY COUNT(Occupation);
